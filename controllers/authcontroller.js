afeb855d4c2db052e0b813bdf5286aa0c3db5082
Abuh Solomon <abuhsolo05@gmail.com>
2022-10-17 09:10:58 +0100

    update

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,7 @@
-import User from "../models/User.js";
+import User from "../models/User.js"; 
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -31,8 +33,18 @@ export const login = async (req, res, next) => {
     if (!isPasswordCorrect)
       return next(createError(404, "Wrong password or username "));
 
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({...otherDetails});
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,27 @@
 import express from "express";
+import {  deleteUser, getUser, getUsers, updateUser } from "../controllers/userController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res, next)=>{
+    res.send("hello user, you are logged in")
+});
 
+router.get("/checkuser/:id", verifyToken, (req, res, next)=>{
+    res.send("hello user, you are logged in and you can delete you account ")
+});
 
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+  
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
 
 export default router;
